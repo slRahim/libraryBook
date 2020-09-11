fa04a2dc90d9a0c65dd6dbbbca34178ff7000b90
abderahim <familleslim1998@gmail.com>
2020-09-11 22:34:58 +0100

    initialize the project

---
+++ android/app/src/main/java/test/librarybook/flutter_library_book/MainActivity.java
@@ -0,0 +1,6 @@
+package test.librarybook.flutter_library_book;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'App.dart';
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
+
+void main() {
+  runApp(MyApp());
+}
